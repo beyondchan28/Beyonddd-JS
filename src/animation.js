c7030aa535131b2ef2362ab520127a334e2e7263
Chandra Gunawan <56698746+beyondchan28@users.noreply.github.com>
2025-01-12 01:00:41 +0700

    Update animation.js

--- src/animation.js
+++ src/animation.js
@@ -10,10 +10,7 @@ function animation_setup() {
 function animation_update(anim) {
 	anim.currentFrame++;
 	let animFrame = Math.floor((anim.currentFrame / anim.speed) % anim.frameCount);
-	console.log(animFrame);
 	let spriteXSSize = anim.sprite.ssize.x;
-	anim.sprite.spos.x = animFrame * spriteXSSize;
-	
-
+	anim.sprite.spos.x = animFrame * spriteXSSize;	
 	draw_image(anim.sprite);
 }
