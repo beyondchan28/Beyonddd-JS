5ca34fe4966cdad50905c9d1878d6fcffc21c505
Chandra Gunawan <56698746+beyondchan28@users.noreply.github.com>
2025-01-31 02:07:26 +0700

    removing assets folder

--- assets/test.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="test" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="icon.png" width="64" height="64"/>
- <tile id="0">
-  <objectgroup draworder="index" id="5">
-   <object id="5" x="0" y="0" width="64" height="64"/>
-  </objectgroup>
- </tile>
-</tileset>
