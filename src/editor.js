37c1d30c80a761dfe74fbdb5d3c9d4a05755fb85
Chandra Gunawan <56698746+beyondchan28@users.noreply.github.com>
2025-02-02 01:07:35 +0700

    implementing labels group and button-labels group

--- src/editor.js
+++ src/editor.js
@@ -1,3 +1,5 @@
+import {is_paused} from './beyonddd.js';
+
 // these 3 are the main editor layout
 const leftPanel = document.getElementById("left-panel");
 const centerPanel = document.getElementById("left-panel");
@@ -37,6 +39,8 @@ function label_create(parent, id, text) {
 	const label = document.createElement('label');
 	label.id = id;
 	label.textContent = text;
+	label.style.display = 'flex';
+	label.style.justifyContent = 'center';
 	parent.appendChild(label);
 	return label;
 }
@@ -65,14 +69,25 @@ function horizontal_box_create(parent, className, id) {
 	if (id !== undefined) {
 		hBox.id = id;
 	}
-	hBox.style.justifyContent = 'space-between';
 	hBox.style.display = "flex";
+	hBox.style.justifyContent = 'space-between';
 	hBox.style.flexDirection = 'row';
 	hBox.style.gap = "10px";
 	hBox.style.padding = "10px";
 	hBox.style.border = "1px solid black";
 	parent.appendChild(hBox);
 	return hBox;
+}
+
+function box_create(parent, className, id) {
+	const box = document.createElement('div');
+	box.style.display = "flex";
+	box.style.justifyContent = 'space-between';
+	box.style.gap = "10px";
+	box.style.padding = "10px";
+	box.style.border = "1px solid black";
+	parent.appendChild(box);
+	return box;
 }
 
 function vertical_box_create(parent, className, id) {
@@ -82,10 +97,13 @@ function vertical_box_create(parent, className, id) {
 		vBox.id = id;
 	}
 	vBox.style.display = "flex";
+	// vBox.style.justifyContent = 'center';
+	// vBox.style.alignItems = 'center';
 	vBox.style.flexDirection = 'column';
 	vBox.style.gap = "10px";
 	vBox.style.padding = "10px";
 	vBox.style.border = "1px solid black";
+	vBox.style.marginBottom = '20px';
 	parent.appendChild(vBox);
 	return vBox;
 }
@@ -98,16 +116,32 @@ function horizontal_button_label_group(hBoxParent, hBoxName, labelText, buttonTe
 	return hBox;
 }
 
+function box_label_group(parent, labelText, detailText) {
+	const box = horizontal_box_create(parent, "label-box");
+	const label = label_create(box, "engine-label", labelText);
+	const detail = label_create(box, "engine-detail", detailText);
+	detail.style.fontWeight = '900';
+	return box;
+}
+
 const sliderParent = div_create(leftPanel, "slider-container")
 const slider = input_slider_create(sliderParent, "slider");
 const span = span_create(sliderParent, "span", "FUCK YOU"); 
 
 
+// Engine states
+const engineStatesV = vertical_box_create(leftPanel, "engine-states");
+const labelName = label_create(engineStatesV,  "engine-states-title","Engine States ⚙️");
+const labelBox = box_label_group(engineStatesV, "Paused", is_paused());
 
-const entityHGroup = horizontal_button_label_group(leftPanel, "entity-add-group", "Add Entity", "Add", () => {
+
+// Game tools
+const gameToolsV = vertical_box_create(leftPanel, "game-tools");
+const label = label_create(gameToolsV, "left-panel-title", "Game Tools 🧰");
+const sceneHGroup = horizontal_button_label_group(gameToolsV, "scene-add-group", "Create Scene", "🏞️", () => {
+	console.log("Create Scene...")
+});
+const entityHGroup = horizontal_button_label_group(gameToolsV, "entity-add-group", "Add Entity", "📦", () => {
 	console.log("Adding Entity...")
 });
 
-const sceneHGroup = horizontal_button_label_group(leftPanel, "scene-add-group", "Create Scene", "Create", () => {
-	console.log("Create Scene...")
-});
