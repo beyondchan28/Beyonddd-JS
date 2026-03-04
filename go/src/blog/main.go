d4c69a72e0360d855aee70f7753e37d3a0393712
Chandra Gunawan <beyonddd.games@gmail.com>
2026-03-04 23:15:45 +0700

    implemented blog list generation

--- go/src/blog/main.go
+++ go/src/blog/main.go
@@ -16,14 +16,59 @@ func (b *Blog) setup() {
 	b.currentPage = (window.Get("location")).Get("pathname").String()
 	fmt.Println("currentPage : ", b.currentPage)
 
+	// NOTE: Set new blog page here
+	b.addPage("page")
+	b.addPage("page")
+	b.addPage("page")
+
 	switch b.currentPage {
-	case "/index.html":
+	case "/":
 		b.front.ReadXDFileWASM("./pages/front.xd")
 		b.front.InsertGeneratedHTML()
-		// case "/blog.html":
+	case "/blog.html":
+		for _, pageData := range b.pages {
+			var titleId int
+			var dateId int
+			for _, pageMap := range pageData.pageMapArray {
+				if v, ok := pageMap[TITLE]; ok {
+					titleId = v[0]
+					fmt.Println("v is: ", v)
+				} else if v, ok := pageMap[DATE]; ok {
+					dateId = v[0]
+				}
+			}
+			b.generateBlogList(pageData.texts[titleId], pageData.texts[dateId])
+		}
 		// TODO: create list for blog page and it must be loaded up based on how much the pages currently are
 
 	}
+}
+
+func (b *Blog) generateBlogList(title, date string) {
+	fmt.Println(title)
+	fmt.Println(date)
+	doc := js.Global().Get("document")
+	blogList := doc.Call("getElementById", "blog-list")
+	blogItem := doc.Call("createElement", "li")
+	blogItem.Set("className", "blog-item")
+
+	link := doc.Call("createElement", "a")
+	link.Set("href", "/")
+	link.Set("textContent", title)
+
+	blogTitle := doc.Call("createElement", "div")
+	blogTitle.Set("className", "blog-title")
+	blogTitle.Call("appendChild", link)
+
+	blogDate := doc.Call("createElement", "div")
+	blogDate.Set("className", "published-date")
+	blogDate.Set("textContent", date)
+
+	blogItem.Call("appendChild", blogTitle)
+	blogItem.Call("appendChild", blogDate)
+
+	blogList.Call("appendChild", blogItem)
+
 }
 
 func (b *Blog) addPage(filename string) {
@@ -36,11 +81,6 @@ func (b *Blog) addPage(filename string) {
 func main() {
 	b := &Blog{}
 	b.setup()
-
-	// NOTE: Set new blog page here
-	b.addPage("page")
-	b.addPage("page")
-	b.addPage("page")
 
 	// go func() {
 	// 	window := js.Global().Get("window")
